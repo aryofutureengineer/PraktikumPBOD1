b66ca90776e147fcfec854b5dd41ff685b170abe
aryoryoiyok <aryopasya321@gmail.com>
2026-05-19 16:11:12 +0700

    Pertemuan 8 Selesai

---
+++ PBO08_24060124140192/src/Anggora.java
@@ -0,0 +1,13 @@
+// Nama File   : Anggora.java
+// Deskripsi   : Realisasi class Anggora sebagai kelas turunan dari Kucing
+// Pembuat     : Aryo Pasya Ramadhan
+// Tanggal     : 28 April 2026
+ 
+public class Anggora extends Kucing {
+    // Atribut
+
+    // Method
+    public Anggora(String nama, double bobot) {
+        super(nama, bobot);
+    }
+}
