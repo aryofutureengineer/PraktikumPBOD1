7e6cd8220c19fc25c18ad05ed4483b7d128b433e
aryoryoiyok <aryopasya321@gmail.com>
2026-05-19 16:05:29 +0700

    Petemuan 5 Selesai

---
+++ Pertemuan5/src/IResize.java
@@ -0,0 +1,11 @@
+// Nama File   : IResize.java
+// Deskripsi   : Interface untuk mengatur operasi resize pada objek bangun datar
+// Pembuat     : Aryo Pasya Ramadhan 
+// Tanggal     : 17 Maret 2026
+
+public interface IResize {
+
+    public void zoomIn();
+    public void zoomOut();
+    public void zoom(int percent);
+}
